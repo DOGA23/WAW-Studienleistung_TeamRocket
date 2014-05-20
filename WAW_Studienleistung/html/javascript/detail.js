690ba10d13ff1ca1c1198a8c607e15ebd98f7655
chridorn <chridorn93@gmail.com>
2014-05-20 16:02:53 +0200

    übung 3
    teile von js, css und die detail.html

---
+++ WAW_Studienleistung/html/javascript/detail.js
@@ -0,0 +1,65 @@
+function teilnehmerTab(){
+    document.getElementById("kosten").style.backgroundColor="transparent";
+    document.getElementById("kosten").style.color="black";
+    document.getElementById("teilnehmer").style.color="whitesmoke";
+    document.getElementById("teilnehmer").style.backgroundColor="blue";
+    document.getElementById("messeAuswahlTable").style.visibility="visible";
+    document.getElementById("messeTables").style.visibility="visible";
+    document.getElementById("tableCebit").style.visibility="visible";
+    document.getElementById("tableCebit").style.backgroundColor="rgb(153,217,234)";
+    document.getElementById("tableConhit").style.visibility="hidden";
+    document.getElementById("tableConhit").style.backgroundColor="transparent";
+    document.getElementById("tableWebtechcon").style.visibility="hidden";
+    document.getElementById("tableWebtechcon").style.backgroundColor="transparent";
+    document.getElementById("tabelleMessekosten").style.visibility="hidden";
+}
+
+function kostenTab(){
+    document.getElementById("kosten").style.backgroundColor="blue";
+    document.getElementById("kosten").style.color="whitesmoke";
+    document.getElementById("messeAuswahlTable").style.visibility="hidden";
+    document.getElementById("teilnehmer").style.color="black";
+    document.getElementById("teilnehmer").style.backgroundColor="transparent";
+    document.getElementById("messeTables").style.visibility="hidden";
+    document.getElementById("tabelleMesseKosten").style.visibility="visible";
+}
+
+function cebitTable(){
+    document.getElementById("cebitTab").style.backgroundColor="deepskyblue";
+    document.getElementById("conhitTab").style.backgroundColor="transparent";
+    document.getElementById("webtechTab").style.backgroundColor="transparent";
+    document.getElementById("tableCebit").style.visibility="visible";
+    document.getElementById("tableCebit").style.backgroundColor="rgb(153,217,234)";
+    document.getElementById("tableConhit").style.visibility="hidden";
+    document.getElementById("tableConhit").style.backgroundColor="transparent";
+    document.getElementById("tableWebtechcon").style.visibility="hidden";
+    document.getElementById("tableWebtechcon").style.backgroundColor="transparent";
+    document.getElementById("tabelleMessekosten").style.visibility="hidden";
+}
+
+function conhitTable(){
+    document.getElementById("conhitTab").style.backgroundColor="deepskyblue";
+    document.getElementById("cebitTab").style.backgroundColor="transparent";
+    document.getElementById("webtechTab").style.backgroundColor="transparent";
+    document.getElementById("tableConhit").style.visibility="visible";
+    document.getElementById("tableConhit").style.backgroundColor="rgb(153,217,234)";
+    document.getElementById("tableCebit").style.visibility="hidden";
+    document.getElementById("tableCebit").style.backgroundColor="transparent";
+    document.getElementById("tableWebtechcon").style.visibility="hidden";
+    document.getElementById("tableWebtechcon").style.backgroundColor="transparent";
+    document.getElementById("tabelleMessekosten").style.visibility="hidden";
+}
+
+function webtechTable(){
+    document.getElementById("webtechTab").style.backgroundColor="deepskyblue";
+    document.getElementById("conhitTab").style.backgroundColor="transparent";
+    document.getElementById("cebitTab").style.backgroundColor="transparent";
+    document.getElementById("tableWebtechcon").style.visibility="visible";
+    document.getElementById("tableWebtechcon").style.backgroundColor="rgb(153,217,234)";
+    document.getElementById("tableConhit").style.visibility="hidden";
+    document.getElementById("tableConhit").style.backgroundColor="transparent";
+    document.getElementById("tableCebit").style.visibility="hidden";
+    document.getElementById("tableCebit").style.backgroundColor="transparent";
+    document.getElementById("tabelleMessekosten").style.visibility="hidden";
+
+}
